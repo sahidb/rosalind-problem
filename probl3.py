e644ca4cfd8c591b82d19f7a3ec901672a88853a
sahidbis <sahid.bismantoko@brin.go.id>
2022-08-26 10:23:44 +0700

    add source for problem 3

---
+++ probl3.py
@@ -0,0 +1,7 @@
+# with open('./dataset/rosalind_rna.txt', 'r') as file:
+#     data = file.read().rstrip()
+dna='AAAACCCGGT'[::-1]
+print(len(dna))
+# for x in dna:
+#     if x == 'A'        x ='T'
+
