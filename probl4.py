9b0cc6fd5e47dc7f914fd6c1d6f4c21963efcf4f
sahidbis <sahid.bismantoko@brin.go.id>
2022-08-27 09:51:40 +0700

    dataset for probl4.py

---
+++ probl4.py
@@ -0,0 +1,15 @@
+'''
+Given: Positive integers n≤40 and k≤5.
+Return: The total number of rabbit pairs that will be present after n
+months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs (instead of only 1 pair).
+'''
+
+def fib(n, k):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1,k)+k*fib(n-2,k)
+
+print(fib(36,5))
