64ef97a9e360b5b48808159866d8982bc142847f
sahidbis <sahid.bismantoko@brin.go.id>
2022-09-16 08:48:20 +0700

    renamed from probl4

---
+++ probl13.py
@@ -0,0 +1,21 @@
+'''
+Given: Six nonnegative integers, each of which does not exceed 20,000. The integers correspond to the number of couples in a population possessing each genotype pairing for a given factor. In order, the six given integers represent the number of couples having the following genotypes:
+
+    AA-AA
+    AA-Aa
+    AA-aa
+    Aa-Aa
+    Aa-aa
+    aa-aa
+
+Return: The expected number of offspring displaying the dominant phenotype in the next generation, under the assumption that every couple has exactly two offspring.
+Sample Dataset
+
+1 0 0 1 0 1
+
+Sample Output
+
+3.5
+
+'''
+
